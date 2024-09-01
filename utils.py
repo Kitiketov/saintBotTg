47db42f657963a248f8c8d61d52e06edaba335a5
Илья Котов <87821623+Kitiketov@users.noreply.github.com>
2024-09-01 14:24:30 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,22 +6,9 @@ def randomize_members(members):
     pairs = {}
     members.sort()
     reciver_list = members.copy()
-
-    if 1513119067 in members and 1338914722 in members:
-        reciver_list.pop(reciver_list.index(1513119067))
-    if 1515123223 in members and 5025889207 in members:
-        reciver_list.pop(reciver_list.index(1515123223))
-
+    
     for member in members:
         while True:
-
-            if member == 1338914722 and 1513119067 in members:
-                reciver = 1513119067
-                break
-            if member == 5025889207 and 1515123223 in members:
-                reciver = 1515123223
-                break
-
             reciver = random.choice(reciver_list)
             if len(reciver_list)==2  and members[-1] in reciver_list:
                 reciver = members[-1]
